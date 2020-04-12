6be5a3c37d6c8c60100e58a576615f460b029ecf
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2020-04-12 16:40:11 -0300

    esqueleto do projeto

--- lib/Home.dart
+++ lib/Home.dart
@@ -11,43 +11,43 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  Future<Map> _getPrice() async {
-    String url = "https://blockchain.info/ticker";
-    http.Response response = await http.get(url);
-    
-    return json.decode(response.body);
+  String _baseUrl = "http://jsonplaceholder.typicode.com/";
+
+  _recoverPost(){
+
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Map>(
-      future: _getPrice(),
-      builder: (context, snapshot){
-        String result;
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-            print('none');
-            break;
-          case ConnectionState.waiting:
-            result = "Carregando...";
-            print('waiting');
-            break;
-          case ConnectionState.active:
-            print('active');
-            break;
-          case ConnectionState.done:
-            print('done');
-            if (snapshot.hasError){
-              result = "Erro ao carregar";
-            } else {
-              double value = snapshot.data["BRL"]["buy"];
-              result = "Preço do bitcoin: ${value.toString()}";
-            }
-            break;
-          default:
-        }
-        return Center(child: Text(result),);
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Future')
+      ),
+      body: FutureBuilder<Map>(
+        future: _recoverPost(),
+        builder: (context, snapshot){
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              print('none');
+              break;
+            case ConnectionState.waiting:
+              print('waiting');
+              break;
+            case ConnectionState.active:
+              print('active');
+              break;
+            case ConnectionState.done:
+              print('done');
+              if (snapshot.hasError){
+              } else {
+                
+              }
+              break;
+            default:
+          }
+          return Center(child: Text(""),);
+        },
+      ),
     );
   }
 }
